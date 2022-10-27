b824539409dd75fd7ced89ce6464c21e740325a2
Carles Julià <111891941+carles-protopixel@users.noreply.github.com>
2022-10-27 10:57:51 +0200

    Update __init__.py
    workaround trying to handle Nuitka issue https://github.com/Nuitka/Nuitka/issues/1861

--- pydantic_argparse/__init__.py
+++ pydantic_argparse/__init__.py
@@ -10,14 +10,19 @@ The public interface exposed by this package is the declarative and typed
 
 # Local
 from .argparse import ArgumentParser
-from .__metadata__ import __title__, __description__, __version__, __author__, __license__
+
+try:
+    from .__metadata__ import __title__, __description__, __version__, __author__, __license__
+    _metadata_exports = (
+        "__title__",
+        "__description__",
+        "__version__",
+        "__author__",
+        "__license__",
+    )
+except ModuleNotFoundError:
+    _metadata_exports = ()
+
 
 # Public Re-Exports
-__all__ = (
-    "ArgumentParser",
-    "__title__",
-    "__description__",
-    "__version__",
-    "__author__",
-    "__license__",
-)
+__all__ = ("ArgumentParser",) + _metadata_exports
