e0817f1ee9fa90f069efced5362cd8a5e9be8d1d
Carles Julià <chaos.ct@gmail.com>
2023-06-16 17:49:52 +0200

    Update __init__.py
    remove conflictive code for nuitka

--- pydantic_argparse/__init__.py
+++ pydantic_argparse/__init__.py
@@ -11,18 +11,4 @@ The public interface exposed by this package is the declarative and typed
 # Local
 from .argparse import ArgumentParser
 
-try:
-    from .__metadata__ import __title__, __description__, __version__, __author__, __license__
-    _metadata_exports = (
-        "__title__",
-        "__description__",
-        "__version__",
-        "__author__",
-        "__license__",
-    )
-except ModuleNotFoundError:
-    _metadata_exports = ()
-
-
-# Public Re-Exports
-__all__ = ("ArgumentParser",) + _metadata_exports
+__all__ = ["ArgumentParser"]
